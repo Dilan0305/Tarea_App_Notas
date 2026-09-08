2486e2df31d34957f876c5a01f74f2fc46ee2d17
dilan0305 <dilan.carranzae@gmail.com>
2026-09-08 08:09:17 -0600

    video_6_segundaParte

--- app/src/main/java/com/example/notasapp_sqlite/AgregarNotaActivity2.kt
+++ app/src/main/java/com/example/notasapp_sqlite/AgregarNotaActivity2.kt
@@ -1,16 +1,18 @@
 package com.example.notasapp_sqlite
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.notasapp_sqlite.databinding.ActivityAgregarNota2Binding
 
 class AgregarNotaActivity2 : AppCompatActivity() {
 
-    private lateinit var bindig :
-
-
+    private lateinit var bindig : ActivityAgregarNota2Binding
+    private lateinit var db : NotasDatabaseHelper
 
 
 
@@ -20,7 +22,25 @@ class AgregarNotaActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_agregar_nota2)
+        bindig = ActivityAgregarNota2Binding.inflate(layoutInflater)
+        db = NotasDatabaseHelper(this)
+
+        bindig.ivGuardarNota.setOnClickListener {
+            val titulo = bindig.etTitulo.text.toString()
+            val descripcion = bindig.etDescripcion.text.toString()
+            val nota = Nota(0, titulo, descripcion)
+            db.insertNota(nota)
+            startActivity(Intent(applicationContext, MainActivity ::class.java))
+            finishAffinity()
+            Toast.makeText(applicationContext, "Se a agregado la nota", Toast.LENGTH_SHORT).show()
+
+        }
+
+
+
+
+        setContentView(bindig.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
