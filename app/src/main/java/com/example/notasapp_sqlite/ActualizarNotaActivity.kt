5b2c583dfcac5b85900c343b9b19655952d0708b
Dilan0305 <dilan.carranzae@gmail.com>
2026-09-10 21:36:53 -0600

    "Video_13_14Y15"
    parte_1_3:18

--- app/src/main/java/com/example/notasapp_sqlite/ActualizarNotaActivity.kt
+++ app/src/main/java/com/example/notasapp_sqlite/ActualizarNotaActivity.kt
@@ -1,20 +1,42 @@
 package com.example.notasapp_sqlite
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.notasapp_sqlite.databinding.ActivityActualizarNotaBinding
 
 class ActualizarNotaActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityActualizarNotaBinding
+    private lateinit var db : NotasDatabaseHelper
+    private var idNota: Int = -1
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_actualizar_nota)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityActualizarNotaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NotasDatabaseHelper(this)
+
+        idNota = intent.getIntExtra("id_nota", -1)
+        if (idNota == -1) {
+            finish()
+            return
+        }
+        val nota = db.getNotaByID(idNota)
+        binding.etTitulo.setText(nota.titulo)
+        binding.etDescripcion.setText(nota.descripcion)
+
+        binding.ivActualizarNota.setOnClickListener {
+            val nuevoTitulo = binding.etTitulo.text.toString()
+            val nuevaDescripcion = binding.etDescripcion.text.toString()
+            val notaActualizada = Nota(idNota, nuevoTitulo, nuevaDescripcion)
+            db.updateNota(notaActualizada)
+            finish()
+            Toast.makeText(this, "Cambios Guardados", Toast.LENGTH_SHORT).show()
         }
     }
 }
